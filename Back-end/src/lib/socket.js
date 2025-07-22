eb6c7094f1f9d75c33b8188b6547c03482281898
Lakshay Joshi <lakshayjoshi67@email.com>
2025-07-22 21:01:26 +0530

    Fixes done

--- Back-end/src/lib/socket.js
+++ Back-end/src/lib/socket.js
@@ -25,7 +25,7 @@ io.on("connection",(socket)=>{
   if(userId) userSocketMap[userId]=socket.id
   io.emit("getOnlineUsers",Object.keys(userSocketMap));
   socket.on("disconnect",()=>{
-    console.log("A usr disconnected",socket.id);
+    console.log("A user disconnected",socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers",Object.keys(userSocketMap));
   });
