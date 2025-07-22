26098ea16724a53d186fd7a3b4d4fe7e3bd8e733
Lakshay Joshi <lakshayjoshi67@email.com>
2025-07-22 22:17:07 +0530

    Fixes made

--- Back-end/src/routes/message.route.js
+++ Back-end/src/routes/message.route.js
@@ -3,9 +3,9 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
-  
+
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
+router.get("chat/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
 
