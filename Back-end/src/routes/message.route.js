dce0a87ce2456c41460318c5bb067f400bc500db
Lakshay Joshi <lakshayjoshi67@email.com>
2025-07-22 16:39:41 +0530

    Your meaningful commit message

--- Back-end/src/routes/message.route.js
+++ Back-end/src/routes/message.route.js
@@ -3,6 +3,6 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router=express.Router();
 router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
+router.get("/chat/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 export default router;
